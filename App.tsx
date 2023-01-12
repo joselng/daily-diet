9a9adee354bf621b1b308d1e4b25e26d265652ec
José Leonardo Nascimento Garcia <leogarcia.net@hotmail.com>
2023-01-12 14:59:25 -0300

    chore: added module-resolver and configure paths

--- App.tsx
+++ App.tsx
@@ -3,7 +3,8 @@ import { ThemeProvider } from 'styled-components';
 import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans';
 
 import theme from './src/theme';
-import { Loading } from './src/components/Loading';
+
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
