3e1175bf3bbaab1f822bfb479c6c9164e21a1c0d
José Leonardo Nascimento Garcia <leogarcia.net@hotmail.com>
2023-01-12 14:24:24 -0300

    chore: created theme colors

---
+++ src/theme/index.ts
@@ -0,0 +1,30 @@
+export default {
+  COLORS: {
+    RED_DARK: '',
+    RED_MID: '',
+    RED_LIGHT: '',
+    GREEN_DARK: '',
+    GREEN_MID: '',
+    GREEN_LIGHT: '',
+    GRAY_700: '#1b1d1e',
+    GRAY_600: '#333638',
+    GRAY_500: '#5c6265',
+    GRAY_400: '#b9bbbc',
+    GRAY_300: '#dddedf',
+    GRAY_200: '#eff0f0',
+    GRAY_100: '#fafafa',
+    WHITE: '#ffffff'
+  },
+  FONT_FAMILY: {
+    REGULAR: 'NunitoSans_400Regular',
+    BOLD: 'NunitoSans_700Bold'
+  },
+  FONT_SIZE: {
+    SM: 12,
+    MD: 14,
+    LG: 16,
+    XL: 18,
+    '2XL': 24,
+    '3XL': 32
+  }
+}
