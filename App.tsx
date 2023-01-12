92c55f6fa95be21157e853fa4e4e9cfd6c938e85
José Leonardo Nascimento Garcia <leogarcia.net@hotmail.com>
2023-01-12 14:48:41 -0300

    chore: added @expo-google-fonts/nunito-sans

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
+import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans';
 
 import theme from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
