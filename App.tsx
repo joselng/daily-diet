2a4dc1a439f2a4502efb58be82ed42866ebe9520
José Leonardo Nascimento Garcia <leogarcia.net@hotmail.com>
2023-01-12 17:36:42 -0300

    chore: initiate creation screen Home

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar, View } from 'react-native';
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans';
 
@@ -6,13 +6,15 @@ import theme from './src/theme';
 
 import { Loading } from '@components/Loading';
 
+import { Home } from '@screens/Home';
+
 export default function App() {
   const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      { fontsLoaded ? <View>Daily Diet</View> : <Loading /> }
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      { fontsLoaded ? <Home /> : <Loading /> }
     </ThemeProvider>
   );
 }
